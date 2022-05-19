6922dd3adb764fe2f4f58349bd5514077e50f711
simonmortensen23 <simon020393@gmail.com>
2022-05-19 15:13:38 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-$("button").mouseenter(function() {
-    $(this).removeClass("make-red").addClass("make-blue")
-})
-$("button").mouseleave(function() {
-    $(this).removeClass("make-blue").addClass("make-red")
+$(document).ready(function() {
+    $(".box").on("click", function() {
+    	/**
+    	 * When we click on an element that has
+    	 * a 'box' class, this event will be fired.
+    	 */
+    	var classNames = $(this).attr("class").split(" ");
+    	var boxClass = classNames[0];
+    	var className = classNames[1];
+    	if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+    		// If this object is already red, turn it black
+    		$("." + className).css("background-color", "#000"); 
+    	} else {
+    		// Else turn all elements with a box class black
+    		// and then change the color of all elements
+    		// with the same class name as the clicked element
+    		// to red.
+    		$("." + boxClass).css("background-color", "#000");
+    		$("." + className).css("background-color", "red");
+    	}
+    });
 })
